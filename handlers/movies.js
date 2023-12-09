8b154020d87b40202a1dcc5972cf438adda213c4
Xin Deng <xind1470@gmail.com>
2023-12-08 17:29:58 -0800

    saving before checking live

--- handlers/movies.js
+++ handlers/movies.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 //3rd party dependencies
 const axios = require("axios");
 
@@ -10,6 +8,8 @@ const MOVIE_ACCESS_TOKEN = process.env.MOVIE_ACCESS_TOKEN;
 
 //internal dependencies
 const Movies = require("../data-models/Movies.js");
+
+const movieCache = {};
 
 
 //actual route handlers
@@ -24,42 +24,52 @@ async function handleMovies(request, response) {
         .json({ error: "Missing required parameters" });
     }
 
-    let movieURL = `https://api.themoviedb.org/3/search/movie`;
+    const movieKey = 'movies-' + city;
 
-    //chat gpt advice// Declare movieResponse outside the if block
-    let movieResponse;  
+    if (movieCache[movieKey] && (Date.now() - movieCache[movieKey].timestamp < 50000)) {
+      console.log('Cache hit');
+      return response.json(movieCache[movieKey].data);
+    } else {
+      console.log('Cache miss');
+      movieCache[movieKey] = {};
+      movieCache[movieKey].timestamp = Date.now();
+      let movieURL = `https://api.themoviedb.org/3/search/movie`;
 
-    if (city) {
-      movieResponse = await axios.get(movieURL, {
-        params: { query: `${city}`,include_adult:'false' },
-        headers: {
-          accept: "application/json",
-          Authorization: `Bearer ${MOVIE_ACCESS_TOKEN}`,
-        },
-      });
-    }
-console.log(movieResponse.data.results);
-    if (movieResponse && movieResponse.data && movieResponse.data.results) {
-      const cityMovies = movieResponse.data.results
-        .slice(0, 20)
-        .sort((a, b) => b.popularity - a.popularity);
-      let sortedMovies = cityMovies.map(
-        (movie) =>
-          new Movies(
-            movie.title,
-            movie.overview,
-            movie.vote_average,
-            movie.vote_count,
-            movie.poster_path,
-            movie.popularity,
-            movie.release_date
-          )
-      );
+       //chat gpt advice// Declare movieResponse outside the if block
+      let movieResponse;
 
-      console.log(sortedMovies);
+      if (city) {
+        movieResponse = await axios.get(movieURL, {
+          params: { query: `${city}`, include_adult: 'false' },
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${MOVIE_ACCESS_TOKEN}`,
+          },
+        });
+      }
 
-      response.json(sortedMovies);
-      console.log(cityMovies);
+      console.log(movieResponse.data.results);
+
+      if (movieResponse && movieResponse.data && movieResponse.data.results) {
+        const cityMovies = movieResponse.data.results
+          .slice(0, 20)
+          .sort((a, b) => b.popularity - a.popularity);
+        let sortedMovies = cityMovies.map(
+          (movie) =>
+            new Movies(
+              movie.title,
+              movie.overview,
+              movie.vote_average,
+              movie.vote_count,
+              movie.poster_path,
+              movie.popularity,
+              movie.release_date
+            )
+        );
+        movieCache[movieKey].data = sortedMovies;
+
+        return response.json(sortedMovies);
+      }
     }
   } catch (error) {
     console.error("Error fetching movie data:", error.message);
